f41373c85006c50db520e7ed961308912da6047d
Brahmananda Bhutia <bhutiabrahmananda@gmail.com>
2023-09-22 15:06:01 +0530

    conversion and operation

--- 01_Basic/03_conversionOperation.js
+++ 01_Basic/03_conversionOperation.js
@@ -4,8 +4,8 @@ let score = "Bunu"
 // console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -27,4 +27,44 @@ let someNumber = 22
 let stringNumber = String(someNumber)
 // console.log(typeof stringNumber);
 // console.log(stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+3);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(3%2);
+
+let str1 = "hello"
+let str2 = " Bunu"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("2" + 3);
+// console.log(2 + "3");
+// console.log("2" + "3");
+// console.log("2" + 3 + 4);
+// console.log( 3 + 4 + "5");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
 
