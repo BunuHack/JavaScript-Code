628919d53ff601e2db1a1035c5bcb9223c9411b2
Brahmananda Bhutia <bhutiabrahmananda@gmail.com>
2023-10-06 14:30:41 +0530

    adding stack and heap example

--- 01_Basic/datatypes_summary.js
+++ 01_Basic/datatypes_summary.js
@@ -36,3 +36,28 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive) , Heap (Non-Primitive)
+
+let myMail = "bunu@gmail.com"
+let myOtherMail = myMail
+
+myOtherMail = "brahma@gmail.com"
+
+console.log(myMail);
+console.log(myOtherMail);
+
+let userOne = {
+    email : "user@gmail.com" ,
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "bunuchanges@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
